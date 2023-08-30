636482026bb0e7bafd61c978d64d8c92e2f56248
TheCrusader1 <dobsong@purdue.edu>
2023-08-30 12:25:46 -0400

    Commit and Sync

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,12 +2,16 @@
 using namespace std;
 int main()
 {
-	float A;
-	float B;
-	float X;
-	cout << "Please enter a value for A: ";
-	cin >> A;
-	cout << "A=" << A << endl;
-	B = (A * 4) + 1;
-	cout << "And B=" << B << endl;
+ float A;
+ float B;
+ float X;
+ cout << "Hello, my name is Ian and I'm going to solve for the equation: " << endl;
+ cout << "Ax + B = 0 " << endl ;
+ cout << "Please enter a value for A: " << endl ;
+ cin >> A;
+ cout << "Please enter a value for B: " << endl;
+ cin >> B;
+ cout << "Solving for " << A << "X + " << B << " = 0 " << endl;
+ cout << "The answer is: " << endl;
+ cout << "X = " << A / -B;
 }
